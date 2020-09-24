4efc265a2628b722aec3cb10c5d291e90fb29dfe
meetdaxini <meetdaxini10@gmail.com>
2020-09-24 14:24:52 +0530

    Fixed bugs when selecting multiple utilities

--- textutils/views.py
+++ textutils/views.py
@@ -52,7 +52,9 @@ def features(request):
 def tutorials(request):
     return render(request, "tutorials.html")
 
+
 def analyzed(request):
+    # get user input
     text = request.POST.get('input', '')
     pr = request.POST.get('pr', 'off')
     cnt = request.POST.get('cnt', 'off')
@@ -61,9 +63,11 @@ def analyzed(request):
     case = request.POST.get('case', 'off')
 
     analyze = text
-    ogCnt = 'Your Original Text is ' + str(charCount(text)) + ' Characters Long.'
+    ogCnt = 'Your Original Text is ' + \
+        str(charCount(text)) + ' Characters Long.'
     anCnt = ''
-
+    # if elif's to analyze text based on user input considering evry case possible
+    # single option selected and case is default
     if pr == 'on' and nlr == 'off' and sr == 'off' and cnt == 'off' and case == 'd':
         analyze = puncRemove(text)
     elif pr == 'off' and nlr == 'on' and sr == 'off' and cnt == 'off' and case == 'd':
@@ -71,22 +75,227 @@ def analyzed(request):
     elif pr == 'off' and nlr == 'off' and sr == 'on' and cnt == 'off' and case == 'd':
         analyze = removeSpace(text)
     elif pr == 'off' and nlr == 'off' and sr == 'off' and cnt == 'on' and case == 'd':
-        anCnt = 'Your Analyzed Text is ' + str(charCount(text)) + ' Characters Long.'
+        anCnt = 'Your Analyzed Text is ' + \
+            str(charCount(analyze)) + ' Characters Long.'
+    # single option selected and case is lower
+    elif pr == 'on' and nlr == 'off' and sr == 'off' and cnt == 'off' and case == 'u':
+        analyze = lowerCase(analyze)
+        analyze = puncRemove(analyze)
+    elif pr == 'off' and nlr == 'on' and sr == 'off' and cnt == 'off' and case == 'u':
+        analyze = lowerCase(analyze)
+        analyze = removeNewLine(analyze)
+    elif pr == 'off' and nlr == 'off' and sr == 'on' and cnt == 'off' and case == 'u':
+        analyze = lowerCase(analyze)
+        analyze = removeSpace(analyze)
+    elif pr == 'off' and nlr == 'off' and sr == 'off' and cnt == 'on' and case == 'u':
+        analyze = lowerCase(analyze)
+        anCnt = 'Your Analyzed Text is ' + \
+            str(charCount(analyze)) + ' Characters Long.'
+    # single option selected and case is upper
+    elif pr == 'on' and nlr == 'off' and sr == 'off' and cnt == 'off' and case == 'u':
+        analyze = upperCase(analyze)
+        analyze = puncRemove(analyze)
+    elif pr == 'off' and nlr == 'on' and sr == 'off' and cnt == 'off' and case == 'u':
+        analyze = upperCase(analyze)
+        analyze = removeNewLine(analyze)
+    elif pr == 'off' and nlr == 'off' and sr == 'on' and cnt == 'off' and case == 'u':
+
+        analyze = upperCase(analyze)
+        analyze = removeSpace(analyze)
+    elif pr == 'off' and nlr == 'off' and sr == 'off' and cnt == 'on' and case == 'u':
+        analyze = upperCase(analyze)
+        anCnt = 'Your Analyzed Text is ' + \
+            str(charCount(analyze)) + ' Characters Long.'
+    # only upper case lowercase default
     elif pr == 'off' and nlr == 'off' and sr == 'off' and cnt == 'off' and case == 'l':
         analyze = lowerCase(text)
     elif pr == 'off' and nlr == 'off' and sr == 'off' and cnt == 'off' and case == 'u':
         analyze = upperCase(text)
     elif pr == 'off' and nlr == 'off' and sr == 'off' and cnt == 'off' and case == 'd':
-        analyze = text
+        pass
+    # two switch case one is cnt plus default case
     elif pr == 'off' and nlr == 'off' and sr == 'on' and cnt == 'on' and case == 'd':
         analyze = removeSpace(text)
-        anCnt = 'Your Analyzed Text is ' + str(charCount(analyze)) + ' Characters Long.'
-    elif pr == 'on' and nlr == 'off' and sr == 'off' and cnt == 'off' and case == 'u':
-        analyze = upperCase(text)
+        anCnt = 'Your Analyzed Text is ' + \
+            str(charCount(analyze)) + ' Characters Long.'
+    elif pr == 'off' and nlr == 'on' and sr == 'off' and cnt == 'on' and case == 'd':
+        analyze = removeNewLine(text)
+        anCnt = 'Your Analyzed Text is ' + \
+            str(charCount(analyze)) + ' Characters Long.'
+    elif pr == 'on' and nlr == 'off' and sr == 'off' and cnt == 'on' and case == 'd':
+        analyze = puncRemove(text)
+        anCnt = 'Your Analyzed Text is ' + \
+            str(charCount(analyze)) + ' Characters Long.'
+    # two switch case one is sr plus default case
+    elif pr == 'off' and nlr == 'on' and sr == 'on' and cnt == 'off' and case == 'd':
+        analyze = removeNewLine(text)
+        analyze = removeSpace(analyze)
+    elif pr == 'on' and nlr == 'off' and sr == 'on' and cnt == 'off' and case == 'd':
+        analyze = puncRemove(text)
+        analyze = removeSpace(analyze)
+    # two switch case one is nlr plus default case
+    elif pr == 'on' and nlr == 'on' and sr == 'off' and cnt == 'off' and case == 'd':
+        analyze = puncRemove(text)
+        analyze = removeNewLine(analyze)
+    # two switch case one is cnt plus upper case
+    elif pr == 'off' and nlr == 'off' and sr == 'on' and cnt == 'on' and case == 'u':
+        analyze = removeSpace(text)
+        anCnt = 'Your Analyzed Text is ' + \
+            str(charCount(analyze)) + ' Characters Long.'
+        analyze = upperCase(analyze)
+
+    elif pr == 'off' and nlr == 'on' and sr == 'off' and cnt == 'on' and case == 'u':
+        analyze = removeNewLine(text)
+        anCnt = 'Your Analyzed Text is ' + \
+            str(charCount(analyze)) + ' Characters Long.'
+        analyze = upperCase(analyze)
+
+    elif pr == 'on' and nlr == 'off' and sr == 'off' and cnt == 'on' and case == 'u':
+        analyze = puncRemove(text)
+        anCnt = 'Your Analyzed Text is ' + \
+            str(charCount(analyze)) + ' Characters Long.'
+        analyze = upperCase(analyze)
+
+    # two switch case one is sr plus upper case
+    elif pr == 'off' and nlr == 'on' and sr == 'on' and cnt == 'off' and case == 'u':
+        analyze = removeNewLine(text)
+        analyze = removeSpace(analyze)
+        analyze = upperCase(analyze)
+
+    elif pr == 'on' and nlr == 'off' and sr == 'on' and cnt == 'off' and case == 'u':
+        analyze = puncRemove(text)
+        analyze = removeSpace(analyze)
+        analyze = upperCase(analyze)
+
+    # two switch case one is nlr plus upper case
+    elif pr == 'on' and nlr == 'on' and sr == 'off' and cnt == 'off' and case == 'u':
+        analyze = puncRemove(text)
+        analyze = removeNewLine(analyze)
+        analyze = upperCase(analyze)
+    # two switch case one is cnt plus lower case
+    elif pr == 'off' and nlr == 'off' and sr == 'on' and cnt == 'on' and case == 'l':
+        analyze = removeSpace(text)
+        anCnt = 'Your Analyzed Text is ' + \
+            str(charCount(analyze)) + ' Characters Long.'
+        analyze = lowerCase(analyze)
+    elif pr == 'off' and nlr == 'on' and sr == 'off' and cnt == 'on' and case == 'l':
+        analyze = removeNewLine(text)
+        anCnt = 'Your Analyzed Text is ' + \
+            str(charCount(analyze)) + ' Characters Long.'
+        analyze = lowerCase(analyze)
+
+    elif pr == 'on' and nlr == 'off' and sr == 'off' and cnt == 'on' and case == 'l':
+        analyze = puncRemove(text)
+        anCnt = 'Your Analyzed Text is ' + \
+            str(charCount(analyze)) + ' Characters Long.'
+        analyze = lowerCase(analyze)
+    # two switch case one is sr plus lower case
+    elif pr == 'off' and nlr == 'on' and sr == 'on' and cnt == 'off' and case == 'l':
+        analyze = removeNewLine(text)
+        analyze = removeSpace(analyze)
+        analyze = lowerCase(analyze)
+
+    elif pr == 'on' and nlr == 'off' and sr == 'on' and cnt == 'off' and case == 'l':
+        analyze = puncRemove(text)
+        analyze = removeSpace(analyze)
+        analyze = lowerCase(analyze)
+
+    # two switch case one is nlr plus lower case
+    elif pr == 'on' and nlr == 'on' and sr == 'off' and cnt == 'off' and case == 'l':
+        analyze = puncRemove(text)
+        analyze = removeNewLine(analyze)
+        analyze = lowerCase(analyze)
+    # three switch case plus default case
+    elif pr == 'off' and nlr == 'on' and sr == 'on' and cnt == 'on' and case == 'd':
+        analyze = removeSpace(text)
+        analyze = removeNewLine(analyze)
+        anCnt = 'Your Analyzed Text is ' + \
+            str(charCount(analyze)) + ' Characters Long.'
+    elif pr == 'on' and nlr == 'on' and sr == 'off' and cnt == 'on' and case == 'd':
+        analyze = removeNewLine(text)
         analyze = puncRemove(analyze)
-
-
+        anCnt = 'Your Analyzed Text is ' + \
+            str(charCount(analyze)) + ' Characters Long.'
+    elif pr == 'on' and nlr == 'off' and sr == 'on' and cnt == 'on' and case == 'd':
+        analyze = puncRemove(analyze)
+        analyze = removeSpace(analyze)
+        anCnt = 'Your Analyzed Text is ' + \
+            str(charCount(analyze)) + ' Characters Long.'
+    elif pr == 'on' and nlr == 'on' and sr == 'on' and cnt == 'off' and case == 'd':
+        analyze = puncRemove(text)
+        analyze = removeSpace(analyze)
+        analyze = removeNewLine(analyze)
+    # three switch case plus upper case
+    elif pr == 'off' and nlr == 'on' and sr == 'on' and cnt == 'on' and case == 'u':
+        analyze = removeSpace(text)
+        analyze = removeNewLine(analyze)
+        anCnt = 'Your Analyzed Text is ' + \
+            str(charCount(analyze)) + ' Characters Long.'
+        analyze = upperCase(analyze)
+    elif pr == 'on' and nlr == 'on' and sr == 'off' and cnt == 'on' and case == 'u':
+        analyze = removeNewLine(analyze)
+        analyze = puncRemove(analyze)
+        anCnt = 'Your Analyzed Text is ' + \
+            str(charCount(analyze)) + ' Characters Long.'
+        analyze = upperCase(analyze)
+    elif pr == 'on' and nlr == 'off' and sr == 'on' and cnt == 'on' and case == 'u':
+        analyze = puncRemove(analyze)
+        analyze = removeSpace(analyze)
+        anCnt = 'Your Analyzed Text is ' + \
+            str(charCount(analyze)) + ' Characters Long.'
+        analyze = upperCase(analyze)
+    elif pr == 'on' and nlr == 'on' and sr == 'on' and cnt == 'off' and case == 'u':
+        analyze = puncRemove(analyze)
+        analyze = removeSpace(analyze)
+        analyze = removeNewLine(analyze)
+        analyze = upperCase(analyze)
+    # three switch case plus lower case
+    elif pr == 'off' and nlr == 'on' and sr == 'on' and cnt == 'on' and case == 'l':
+        analyze = removeSpace(text)
+        analyze = removeNewLine(analyze)
+        anCnt = 'Your Analyzed Text is ' + \
+            str(charCount(analyze)) + ' Characters Long.'
+        analyze = lowerCase(analyze)
+    elif pr == 'on' and nlr == 'on' and sr == 'off' and cnt == 'on' and case == 'l':
+        analyze = removeNewLine(text)
+        analyze = puncRemove(analyze)
+        anCnt = 'Your Analyzed Text is ' + \
+            str(charCount(analyze)) + ' Characters Long.'
+        analyze = lowerCase(analyze)
+    elif pr == 'on' and nlr == 'off' and sr == 'on' and cnt == 'on' and case == 'l':
+        analyze = puncRemove(analyze)
+        analyze = removeSpace(analyze)
+        anCnt = 'Your Analyzed Text is ' + \
+            str(charCount(analyze)) + ' Characters Long.'
+        analyze = lowerCase(analyze)
+    elif pr == 'on' and nlr == 'on' and sr == 'on' and cnt == 'off' and case == 'l':
+        analyze = puncRemove(text)
+        analyze = removeSpace(analyze)
+        analyze = removeNewLine(analyze)
+        analyze = lowerCase(analyze)
+    # four switch i.e all plus default or upper or lower
+    elif pr == 'on' and nlr == 'on' and sr == 'on' and cnt == 'on' and case == 'l':
+        analyze = puncRemove(analyze)
+        analyze = removeSpace(analyze)
+        analyze = removeNewLine(analyze)
+        anCnt = 'Your Analyzed Text is ' + \
+            str(charCount(analyze)) + ' Characters Long.'
+        analyze = lowerCase(analyze)
+    elif pr == 'on' and nlr == 'on' and sr == 'on' and cnt == 'on' and case == 'u':
+        analyze = puncRemove(analyze)
+        analyze = removeSpace(analyze)
+        analyze = removeNewLine(analyze)
+        anCnt = 'Your Analyzed Text is ' + \
+            str(charCount(analyze)) + ' Characters Long.'
+        analyze = upperCase(analyze)
+    elif pr == 'on' and nlr == 'on' and sr == 'on' and cnt == 'on' and case == 'd':
+        analyze = puncRemove(analyze)
+        analyze = removeSpace(analyze)
+        analyze = removeNewLine(analyze)
+        anCnt = 'Your Analyzed Text is ' + \
+            str(charCount(analyze)) + ' Characters Long.'
     else:
-        analyze = 'else'
-    params = {"anText": analyze, "ogText": text, 'anCnt': anCnt, 'ogCnt': ogCnt}
+        analyze = 'error'
+    params = {"anText": analyze, "ogText": text,
+              'anCnt': anCnt, 'ogCnt': ogCnt}
     return render(request, 'analyzed.html', params)
